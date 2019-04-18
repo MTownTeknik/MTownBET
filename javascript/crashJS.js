b4e44e681d8a4dc363b69a6572dcb3a4e8815bb1
Adam Palm <adpa01@LT007646.edu.ad.monsteras.se>
2019-04-18 10:17:17 +0200

    Lade till lite grejer som får den att crasha.

--- javascript/crashJS.js
+++ javascript/crashJS.js
@@ -1,4 +1,43 @@
-function test() {
+var interval = setInterval(crasher, 100);
+var stop = false
+var randomNum;
+var fcnt = 0; //function time
+var xPos = 10;
+var yPos = 10;
+
+randomNum = Math.floor((Math.random() * 100) + fcnt);
+console.log(fcnt + " " + randomNum);
+
+function crashCounter() {
+  var multiplier = 1.00;
+  var i;
+
+  for (i = 0; crasher()!= 1; i++) {
+    graph();
+    multiplier = multiplier + 0.01;
+    console.log(multiplier);
+  } //doesnt draw the line down when crashing
+}
+
+function crasher() {
+  fcnt++;
+
+  if (fcnt >= randomNum) {
+    stop = true;
+    clearInterval(interval);
+    return 1;
+  }
+}
+
+function graph() {
   var ctx = document.getElementById("canv").getContext("2d");
-  ctx.fillRect(10,10,1,1);
+  ctx.fillStyle = "#FFF";
+
+  if (stop == false) {
+    ctx.fillRect(xPos, yPos, 1, 1);
+    xPos++;
+  } else if (stop == true){
+    ctx.fillRect(xPos, yPos, 1, 50);
+  }
+  ctx.save();
 }
