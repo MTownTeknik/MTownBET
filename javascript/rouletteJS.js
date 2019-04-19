dd1d0e2b4ecc3797c8274b7b9bbba9258944d195
Henrik Nilsson <heni01@LT007642.edu.ad.monsteras.se>
2019-04-19 11:48:35 +0200

    Visuell uppgradering av Roulette + sifferOrdning

--- javascript/rouletteJS.js
+++ javascript/rouletteJS.js
@@ -1,23 +1,37 @@
 
+var redColor = "#ff3855";
+var blackColor = "#232323";
+var greenColor = "#00bb00";
+
+var numOrder = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 
+				5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
+
+test();
 
 function test() {
   	var ctx = document.getElementById("canv").getContext("2d");
 
   	for(var i = 0; i < 37; i++)
   	{
-  		if(i%2 == 0)
-  		{
-  			ctx.fillStyle = "#FF0000";
-  		} else {
-  			ctx.fillStyle = "#000000";
-  		}
 
-		
+  		if(i%2 == 1)
+  		{
+  			ctx.fillStyle = redColor;
+  		} else {
+  			ctx.fillStyle = blackColor;
+  		}
+		if(i == 0) 
+		{
+			ctx.fillStyle = greenColor;
+		}
+
+
   		ctx.fillRect(i * 100, 0, 100, 150);
-  		ctx.fillStyle = "black";
+
+		ctx.fillStyle = "#FFF";
 		ctx.textAlign = "center";
 		ctx.font = "40px Calibri";
-  		ctx.fillText("27", i * 100 + 50, 53);
+  		ctx.fillText(numOrder[i], i * 100 + 50, 53);
   	}
 
   	ctx.fillStyle = "#5994d7";
