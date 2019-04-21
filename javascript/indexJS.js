7158e138bd4e03db03de1b85389957d4bea2cb56
Henrik Nilsson <heni01@LT007642.edu.ad.monsteras.se>
2019-04-21 13:08:13 +0200

    Laddningsscreen vid start (1st iteration)

--- javascript/indexJS.js
+++ javascript/indexJS.js
@@ -1,4 +1,12 @@
+var loadRepeats = 3;
+
+
+
+loadAnim = setInterval(removeLoad, 1000);
+
+
 function main() {
+
 }
 
 function loadPage(url, pix) {
@@ -6,4 +14,15 @@ function loadPage(url, pix) {
 	var frame = document.getElementById("gameFrame");
 	frame.src = url;
 	frame.style.height = pix + 'vw';
+}
+
+function removeLoad() {
+	loadRepeats--;
+
+	if(loadRepeats == 0) {
+		var overlay = document.getElementById("preload");
+		overlay.parentNode.removeChild(overlay);
+		loadPage("roulette.html", 50);									//BYT ROULETTE MED HOME.HTML NÄR DEN ÄR GJORD!!!
+		clearInterval(loadAnim);
+	}
 }
