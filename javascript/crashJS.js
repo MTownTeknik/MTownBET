66666a425fdc3fca81b44820a9ed2d4efcea8657
Adam Palm <adpa01@LT007646.edu.ad.monsteras.se>
2019-05-02 09:52:18 +0200

    Grafen är mer smooth och oddsen är bättre.

--- javascript/crashJS.js
+++ javascript/crashJS.js
@@ -1,23 +1,22 @@
-var interval = setInterval(crasher, 100);
+var interval = setInterval(crasher, 10);
 var stop = false
 var randomNum;
 var fcnt = 0; //function time
 var xPos = 0;
-var yPos = 390;
+var yPos = 400;
 
-randomNum = Math.floor((Math.random() * 100) + fcnt);
+randomNum = Math.floor((Math.random() * 200) + fcnt);
 console.log(fcnt + " " + randomNum);
 
 function crashCounter() {
   var multiplier = 1.00;
   var i;
 
-  for (i = 0; crasher()!= 1; i++) {
-    graph();
+  for (i = 0; crasher() != 1; i++) {
     multiplier = multiplier + 0.01;
     console.log(multiplier);
   }
-  graph();
+
 }
 
 function crasher() {
@@ -26,8 +25,9 @@ function crasher() {
   if (fcnt >= randomNum) {
     stop = true;
     clearInterval(interval);
-    return 1;
+    //return 1;
   }
+  graph();
 }
 
 function graph() {
@@ -35,11 +35,17 @@ function graph() {
   ctx.fillStyle = "#FFF"
 
   if (stop == false) {
-    ctx.fillRect(xPos, yPos, 5, 10);
-    xPos += 5;
-    yPos -= 3;
-  } else if (stop == true){
-    ctx.fillRect(xPos, yPos, 7, 1000);
+    ctx.fillRect(xPos, yPos, 5, 5);
+    xPos += 1;
+    yPos -= 1;
+  } else if (stop == true) {
+    ctx.fillRect(xPos, yPos, 5, 1000);
   }
   ctx.save();
+}
+
+function clean() { //clears the canvas
+  var ctx = document.getElementById("canv").getContext("2d");
+
+  ctx.clearRect(0, 0, 480, 400);
 }
