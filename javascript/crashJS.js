51bf7bfbdcc5e70866bc157b707801b5df55e72e
Adam Palm <adpa01@LT007646.edu.ad.monsteras.se>
2019-05-16 10:05:27 +0200

    Lade till en bet knapp och lite JS & CSS till den.

--- javascript/crashJS.js
+++ javascript/crashJS.js
@@ -1,11 +1,16 @@
-var interval = setInterval(crasher, 8);
-var stop = false
-var randomNum;
+var stop = false;
+var iteration = 0;
 var fcnt = 0; //function time
 var xPos = 0;
 var yPos = 400;
 var multiplier = 1.00;
-var iteration = 0;
+var interval;
+var randomNum;
+
+var invested = 0;
+var buttonPressed = 0;
+var pullOut = false;
+var able2pullOut = false;
 
 randomNum = Math.floor((Math.random() * 200));
 
@@ -31,11 +36,11 @@ function graph() {
   var flooredMultiplier = Math.floor(multiplier);
 
   if (stop == false) {
-    ctx.fillRect(xPos/1.7, yPos - 3, 3, 3);
+    ctx.fillRect(xPos / 1.7, yPos - 3, 3, 3);
     xPos += 1;
     yPos -= Math.pow(xPos, 1.1) * 0.0007;
   } else if (stop == true) {
-    ctx.fillRect(xPos/1.7, yPos - 3, 3, 1000);
+    ctx.fillRect(xPos / 1.7, yPos - 3, 3, 1000);
   }
 
   ctx.fillStyle = "#2f3e45";
@@ -44,4 +49,31 @@ function graph() {
   ctx.font = "30px Arial";
   ctx.fillStyle = "#FFF"
   ctx.fillText(multiplier + "X", 80, 110);
+}
+
+function bet() {
+  interval = setInterval(crasher, 8);
+  invested = currentBet;
+  buttonPressed++;
+  maxCurrency -= invested;
+  setCookie("Currency", maxCurrency, 7);
+
+  if (buttonpressed = 1) {
+    able2pullOut = true;
+  }
+
+  if (buttonPressed = 2 && able2pullOut == true && stop == false) {
+    pullOut = true;
+    able2pullOut = false;
+  }
+
+  if (pullOut == true && stop == false) {
+    maxCurrency += (invested * multiplier);
+  }
+  //this if resets everything
+  if (stop == true && able2pullOut == false) {
+    buttonPressed = 0;
+    able2pullOut = false;
+    stop = false
+  }
 }
